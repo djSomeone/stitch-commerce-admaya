357413922c92a428161ee124f64120208ab80afb
djSomeone <gauravkumarprajapati496@gmail.com>
2025-02-14 10:26:45 +0530

    increased the size of the request data size

--- server.js
+++ server.js
@@ -33,6 +33,8 @@ mongoose.connect(process.env.MONGO_URL, {
     console.log(err);
 });
 
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ limit: "50mb", extended: true }));
 
 
 // for loging the rq,rs
